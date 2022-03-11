7b1553fd4c95337ddf5ff7daab3681d89f49bc6a
Jason <jason.obeid98@gmail.com>
2022-03-10 20:42:09 -0500

    Add custom icon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,6 +4,7 @@ import "../styles/globals.css";
 import LogRocket from "logrocket";
 import React from "react";
 import { AuthContextProvider } from "../src/context/AuthContext";
+import Head from "next/head";
 
 if (typeof window !== "undefined" && process.env.NODE_ENV === "production") {
   LogRocket.init("ecxlpy/tweeter");
@@ -14,6 +15,30 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
+        <Head>
+          <title>Tweeter</title>
+          <link
+            rel="apple-touch-icon"
+            sizes="180x180"
+            href="/apple-touch-icon.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+            href="/favicon-32x32.png"
+          />
+          <link
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+            href="/favicon-16x16.png"
+          />
+          <link rel="manifest" href="/site.webmanifest" />
+          <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+          <meta name="msapplication-TileColor" content="#da532c" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <Component {...pageProps} />
       </AuthContextProvider>
     </QueryClientProvider>
